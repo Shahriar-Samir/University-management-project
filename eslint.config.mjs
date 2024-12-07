da064ff37939553e4d0952e6f6f74cbdcd7cff3b
Shahriar-Samir <shabusiness035@gmail.com>
2024-12-07 16:59:34 +0600

    add: user model

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,30 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import jsPlugin from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierConfig from 'eslint-config-prettier';
+import globals from 'globals';
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...eslintPluginPrettierRecommended,
   {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
     rules: {
+      ...jsPlugin.configs.recommended.rules, // Spread JS recommended rules
+      ...tsPlugin.configs.recommended.rules, // Spread TypeScript recommended rules
+      ...prettierConfig.rules, // Spread Prettier rules
       'no-unused-vars': 'error',
       'prefer-const': 'error',
       'no-console': 'warn',
     },
-    ignores: ['.node_modules/*', 'dist'],
+    ignores: ['node_modules/', 'dist/'], // Patterns to ignore
   },
-]
+];
--- src/modules/users/users.router.ts
+++ src/modules/users/users.router.ts
@@ -1,6 +1,39 @@
-import { Schema } from "mongoose";
-import { TUser } from "./users.interface";
+import { model, Schema } from 'mongoose';
+import { TUser } from './users.interface';
 
-const userSchema = new Schema<TUser>({
-    id:
-})
+const userSchema = new Schema<TUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'faculty', 'student'],
+    },
+    status: {
+      type: String,
+      enum: ['ín-progress', 'blocked'],
+    },
+    isDeleted: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = model<TUser>('User', userSchema);
+
+export default User;
